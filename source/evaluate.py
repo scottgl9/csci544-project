6c47c515ddfc409defc8dd7e38337a9ee6cb5125
aditya-anulekh <adityaan@usc.edu>
2023-04-23 23:56:00 -0700

    Added evaluation code

---
+++ source/evaluate.py
@@ -0,0 +1,16 @@
+from metrics.stereoset import evaluate_stereoset_intrasentence
+from metrics.crows_pairs import evaluate_crows_pairs
+from utils import convert_to_hf_model
+from config import DEVICE
+
+
+def evaluate(ckpt_path, dataset):
+    model = convert_to_hf_model(ckpt_path)
+    model = model.to(DEVICE)
+    if dataset == "stereoset":
+        evaluate_stereoset_intrasentence(model, device=DEVICE)
+    elif dataset == "crows":
+        evaluate_crows_pairs(model, device=DEVICE)
+    else:
+        raise NotImplementedError("dataset should be either streoset or crows")
+    return
